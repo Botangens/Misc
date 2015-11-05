67f6f224b7e76f97f8ff9a98fd43ae14a1c54335
Gleb <kgill-leebr@ukr.net>
2015-11-05 16:14:11 +0200

    Create main.cpp

---
+++ Core/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File: main.cpp
+ * Created: November 5, 2015
+ * Contains: Main loop
+ */
+ 
+//------------------------ standart includes -------------------------
+ 
+//------------------------ external includes -------------------------
+ 
+//------------------------ project includes --------------------------
+#include "CoreTypes.hpp"
+ 
+//------------------------------- MAIN -------------------------------
+int main( int argc, char* argv[] )
+{
+  while( 0 )  // <- Actually the main loop
+  {
+  }
+  
+  return 0;
+}
