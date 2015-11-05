795b12606b112f9abebdc7e73fae5895d2e92ff9
Gleb <kgill-leebr@ukr.net>
2015-11-05 16:30:21 +0200

    Create CoreTypes.hpp

---
+++ Core/CoreTypes.hpp
@@ -0,0 +1,19 @@
+/* 
+ * File: main.cpp
+ * Created: November 5, 2015
+ * Contains: Custom core types definition
+ */
+ 
+ //------------------------ instance types ---------------------------
+ 
+ typedef int integer;  // core type for integral values
+ typedef float real;  // core type for fractional values
+ typedef size_t index;  // core type for sizes and indexes
+ 
+ //------------------------- pointer types ----------------------------
+ 
+ typedef integral* iptr;  // core type for integral pointers
+ typedef real* rptr;  // core type for real pointers
+ typedef index* indptr;  // core type for size and index pointers
+ typedef void* vptr;  // core type for typeless pointers
+ 
