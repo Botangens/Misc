c287612928ad33fe5654fda2a1dfcf7055c446b9
Gleb <kgill-leebr@ukr.net>
2016-02-18 14:21:15 +0200

    more cunning instantiations!

--- Examples&Tutorials/new_quiz.cpp
+++ Examples&Tutorials/new_quiz.cpp
@@ -3,40 +3,49 @@
 using namespace std;
 
 // simply for nice printing
-template<class X>
-void t_print(const X& x )
-{
-  cout<< *(int*)(& x )<<"\t"<<typeid( x ).name()<<endl;
+#define T_PRINT( X ) \
+{ \
+  cout<< #X << ":\t" << *(int*)(& X ) << "\t" \
+    << &X << "\t" << typeid( X ).name() << endl; \
 }
 
 int main()
 {
+  struct { int operator()(){return {};} } i;
 
-  int i0, i1 {}, i2 = {}, i3(), i4 = int();
+  int i0, i1 {}, i2 = {}, i3(), i4 = int(), i5 = i(), i6 = i3();
 
   auto ai2 = i2;
   auto ai3 = i3;
   auto ai4 = i4;
 
-  t_print( i0 );
-  t_print( i1 );
-  t_print( i2 );
-  t_print( i3 );
-  t_print( i4 );
+  cout << "name\tval\taddress\t\ttypename" << endl << endl;
+
+  T_PRINT( i0 );
+  T_PRINT( i1 );
+  T_PRINT( i2 );
+  T_PRINT( i3 );
+  T_PRINT( i4 );
+  T_PRINT( i5 );
+  T_PRINT( i6 );
+  T_PRINT( i );
 
   cout<<endl;
 
-  t_print( ai2 );
-  t_print( ai3 );
-  t_print( ai4 );
+  T_PRINT( ai2 );
+  T_PRINT( ai3 );
+  T_PRINT( ai4 );
 
   // predict the output!!
   cin.get();
   return 0;
 }
 
-int i0() { return {}; }
-int i1() { return {}; }
-int i2() { return {}; }
-int i3() { return {}; }
-int i4() { return {}; }
+int i0() { return {10}; }
+int i1() { return {11}; }
+int i2() { return {12}; }
+int i3() { return {13}; }
+int i4() { return {14}; }
+int i5() { return {15}; }
+int i6() { return {16}; }
+int i() { return {101}; }
