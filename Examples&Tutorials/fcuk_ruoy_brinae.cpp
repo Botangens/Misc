034cb49ded6b556924927cd3728e30a5aeb26004
Gleb <kgill-leebr@ukr.net>
2017-06-14 14:56:17 +0300

    Create fcuk_ruoy_brinae.cpp
    MOAR BRAINF@C#!NG

---
+++ Examples&Tutorials/fcuk_ruoy_brinae.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+// Just an esoteric program - you may try to comprehend what is going on here.
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CASE 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+struct Outer1
+{
+  struct Inner1;
+
+  Inner1& i1;
+};
+
+struct Outer1::Inner1
+{
+  Outer1 o1;
+  static const int v{-1};
+};
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CASE 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+struct Outer2
+{
+  struct Inner2;
+
+  Inner2& i2;
+  const int& v;
+};
+
+struct Outer2::Inner2
+{
+  Outer2 o2;
+};
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CASE 3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+struct Outer3
+{
+  struct Inner3;
+
+  Inner3& i3;
+  const int& v;
+
+  Outer3(Inner3& r);
+};
+
+struct Outer3::Inner3
+{
+  friend struct Outer3;
+private:
+  static const int v;
+
+public:
+  Outer3 o3;
+
+  Inner3(Outer3&& r): o3(r) {}
+};
+
+const int Outer3::Inner3::v {-3};
+
+Outer3::Outer3(Inner3& r): i3(r), v(r.v){}
+
+// ============================== MAIN ======================================
+
+int main()
+{
+  // ------------- 1 ------------------
+
+  Outer1::Inner1 i1{i1};
+
+  cout<<i1.o1.i1.o1.i1.o1.i1.v<<endl;
+
+  // ------------- 2 ------------------
+
+  Outer2::Inner2 i2{{i2, -2}};
+
+  cout<<i2.o2.i2.o2.i2.o2.v<<endl;
+
+  // ------------- 3 ------------------
+
+  Outer3::Inner3 i3{{i3}};
+
+  cout<<i3.o3.i3.o3.i3.o3.v<<endl;
+
+  cout<<"_____________________"<<endl;
+}
